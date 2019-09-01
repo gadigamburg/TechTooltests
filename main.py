5e235bd89d7e11142bc865ded4700bbfe7b67712
rammshtein <47314848+rammshtein@users.noreply.github.com>
2019-09-01 18:37:15 +0300

    create test suite

--- main.py
+++ main.py
@@ -1,54 +1,55 @@
 from selenium import webdriver
 import os,sys
 from ConfigParser import SafeConfigParser
+import unittest
+import time
+
 
 ###  Global content for all project ######
 #path of Chromedriver
 dir=os.getcwd()
 chrome_driver_path=dir + "\chromedriver.exe"
-ip=str(sys.argv[1])
-device_ip=ip
+
 config_parser=SafeConfigParser()
 config_parser.read('config.properties')
+device_ip=config_parser.get('unit_parameters','ip')
+
 ###########################################
 
+class  LoginTests(unittest.TestCase):
+    def setUp(self):
+    # Create a new Chrome session
+       self.driver=webdriver.Chrome(chrome_driver_path)
+       self.driver.implicitly_wait(30)
+       self.driver.maximize_window()
+        ###################################
+       self.admintool_url=str("http://"+device_ip+":9000")
+        # navigate to the application page
+       self.driver.get(self.admintool_url)
 
-# Create a new Chrome session
-driver=webdriver.Chrome(chrome_driver_path)
-driver.implicitly_wait(30)
-driver.maximize_window()
-###################################
+    def test_correct_login(self):
+        ## get username field textbox
+        self.username_field=self.driver.find_element_by_id("input-username")
+        self.username_field.clear()
+        ## enter username to field
+        self.admin_user=config_parser.get('username_passwords','general_user_admin')
+        self.username_field.send_keys(self.admin_user)
 
+        ## enter password  text field
+        self.password_field=self.driver.find_element_by_id("input-password")
+        self.password_field.clear()
 
-###  Star Log In Test Case 1
-### with correct details
-admintool_url=str("http://"+device_ip+":9000")
-# navigate to the application page
-driver.get(admintool_url)
-
-
-## get username field textbox
-username_field=driver.find_element_by_id("input-username")
-username_field.clear()
-
-## enter username to field
-admin_user=config_parser.get('username_passwords','general_user_admin')
-username_field.send_keys(admin_user)
-
-
-## enter password  text field
-password_field=driver.find_element_by_id("input-password")
-password_field.clear()
-
-## enter password to field
-password_admin_user=config_parser.get('username_passwords','general_password_admin')
-password_field.send_keys(password_admin_user)
-driver.implicitly_wait(5)
-### Pres on 'Log in' ###
-#log_in_btn=driver.find_element_by_class_name("btn-success btn-full-width")
-log_in_btn=driver.find_element_by_xpath("//*[contains(text(),'Log In')]")
-log_in_btn.submit()
-
+        ## enter password to field
+        self.password_admin_user=config_parser.get('username_passwords','general_password_admin')
+        self.password_field.send_keys(self.password_admin_user)
+        ### Pres on 'Log in' ###
+        #self.log_in_btn=self.driver.find_element_by_class_name("btn-success btn-full-width")
+        self.log_in_btn=self.driver.find_element_by_xpath("//*[contains(text(),'Log In')]")
+        self.log_in_btn.submit()
+        time.sleep(10)
+    def tearDown(self):
+        # close the browser window
+        self.driver.quit()
 #####################################
 
 
@@ -56,5 +57,6 @@ log_in_btn.submit()
 #driver.quit()
 
 
-
+if __name__=='__main__':
+    unittest.main(verbosity=2)
 
